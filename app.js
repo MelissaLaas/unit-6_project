57a897a90b24ce87cefd0e11ce93722e9e0f42fa
MelissaLaas <melissa.laas16@gmail.com>
2021-06-21 13:37:31 +0400

    added checkWin function

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
-const qwerty = document.getElementById('qwerty');
-const phrase = document.getElementById('phrase');
+const qwerty = document.querySelector('#qwerty');
+const phrase = document.querySelector('#phrase');
 let missed = 0;
 
 const startButton = document.querySelector('.btn__reset');
@@ -8,18 +8,18 @@ const overlay = document.querySelector('#overlay');
 //Start game button
 startButton.addEventListener('click', () => {
     overlay.style.display = 'none';
-    // const phraseArray = getRandomPhraseAsArray(phrases);
-    // addPhraseToDisplay(phraseArray);
+    const phraseArray = getRandomPhraseAsArray(phrases);
+    addPhraseToDisplay(phraseArray);
 });
 
 //Phrases array
 const ul = phrase.querySelector('ul');
 const phrases = [
-    "Learn to rest not to quit",
-    "Everything is a choice",
-    "Believe in yourself",
-    "Enjoy life",
-    "I love JavaScript"
+    'Learn to rest not to quit',
+    'Everything is a choice',
+    'Believe in yourself',
+    'Enjoy life',
+    'I love JavaScript'
 ];
 
 //Functions to split the array phrases into strings
@@ -51,7 +51,7 @@ function addPhraseToDisplay(arr){
 }
 
 //Create a checkLetter function.
-let button = document.getElementsByTagName('button');
+const button = document.getElementsByTagName('button');
 
 function checkLetter(button) {
     const letters = document.querySelectorAll('#phrase li');
@@ -59,8 +59,9 @@ function checkLetter(button) {
 
     const chosenLetter = button.textContent;
     for (let i = 0; i < letters.length; i++){
-        const li = letters[i]
-        if ( chosenLetter === li.textContent.toLowerCase()) {
+        const li = letters[i];
+
+        if (chosenLetter === li.textContent.toLowerCase()) {
             letters[i].className = "show";
             match = chosenLetter;
         }
@@ -71,23 +72,34 @@ function checkLetter(button) {
 //Add an event listener to the keyboard.
 qwerty.addEventListener('click', (e) => {
     let button = e.target;
-    if (e.target.tagName === 'BUTTON' || e.target.className === 'chosen') {
+
+    if (button.className === 'BUTTON' || button.className === 'chosen'){
         return null;
     }
-
-    button.className = "chosen";
-    button.disabled = true;
-    let check = checkLetter(button);
-    const heart = document.querySelectorAll('.tries img');
-
-    if (check === null) {
-        heart[missed].src = 'images/lostHeart.png';
-        missed += 1;
+        button.className = 'chosen';
+        button.disabled = true;
+        const check = checkLetter(button);
+        const heart = document.querySelectorAll('.tries img');
+    
+        if (check === null) {
+            heart[missed].src = 'images/lostHeart.png';
+            missed +=1;
     }
     checkWin();
 }); 
 
-//Count the missed guesses in the game.
-
-
 //Create a checkWin function.
+function checkWin() {
+    const letterClass = document.querySelectorAll('.letter');
+    const showClass = document.querySelectorAll('.show');
+
+    if(letterClass.length === showClass.length) {
+        overlay.className = 'win';
+        overlay.textContent = 'Congratulations! You are the winner!';
+        overlay.style.display = 'flex';
+    } else if (missed > 4){
+        overlay.className = 'lose';
+        overlay.textContent = 'Sorry, you loose...';
+        overlay.style.display = 'flex';
+    }
+};
